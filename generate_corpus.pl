484f9243f7133641663aa941a28d7548d361cab8
Peter Sergeant <pete@clueball.com>
2011-03-31 23:13:19 +0100

    Corpus

---
+++ generate_corpus.pl
@@ -0,0 +1,66 @@
+#!perl
+
+use strict;
+use warnings;
+
+use YAML::XS;
+use URI::Sequin;
+use URI::ParseSearchString;
+use URI::ParseSearchString::Heuristic;
+use URI::ParseSearchString::More;
+
+my @cases;
+my $uparse = new URI::ParseSearchString;
+my $more = new URI::ParseSearchString::More;
+
+*URI::ParseSearchString::More::get_mech = sub { fakemech->new() };
+
+while (my $line = <STDIN>) {
+	chomp $line;
+	next unless $line;
+	$line .= "http://$line" if index($line, 'http');
+
+	my $output = {
+		url   => $line,
+		terms => '',
+		name  => '',
+		full_name => '',
+	};
+
+	$output->{'guess'}->{'sequin'} = {
+		terms => (URI::Sequin::key_extract( $line ) || ''),
+		name  => (URI::Sequin::se_extract(  $line ) || '')
+	};
+
+	$output->{'guess'}->{'pss'} = {
+		terms => ( $uparse->se_term( $line ) || '' ),
+		name  => ( $uparse->se_name( $line ) || '' )
+	};
+
+	$output->{'guess'}->{'pss_m'} = {
+		terms => (
+			$more->se_term( $line ) ||
+			$more->guess( $line )   ||
+			'' ),
+		name  => ( $more->se_name( $line ) || '' )
+	};
+
+	my $hresult = URI::ParseSearchString::Heuristic->parse( $line );
+	$output->{'guess'}->{'heuristic'} = {};
+	if ( $hresult ) {
+		$output->{'terms'} = $hresult->{'search_terms'} || '';
+		$output->{'name'} = $hresult->{'engine_simple_name'} || '';
+		$output->{'full_name'} = $hresult->{'engine_full_name'} || '';
+	}
+
+	push( @cases, $output );
+
+}
+
+print Dump \@cases;
+
+package fakemech;
+sub new    { bless {}, 'fakemech' }
+sub get    {''}
+sub status {''}
+sub title  {''}
